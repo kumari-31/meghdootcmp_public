f8a4998336542898408463de572b290a86035d4a
bhavna <sortebhavna08@gmail.com>
2026-01-09 09:31:31 +0530

    9jan

--- Backend/Unified_Proect_Api/Unified_Proect_Api/settings.py
+++ Backend/Unified_Proect_Api/Unified_Proect_Api/settings.py
@@ -173,7 +173,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-OTP_TEST_MODE = True
+# OTP_TEST_MODE = True
+# MFA behavior
+MFA_MODE = os.getenv("MFA_MODE", "NONE")
+# EMAIL | TOTP | NONE
+
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
